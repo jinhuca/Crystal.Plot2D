233a9e5ae548a1f1dc5810f62eae6b4d5307eb9b
jinhuca <jinhuca@gmail.com>
2021-11-18 14:29:33 -0500

    77

---
+++ Samples/S001LineGraph/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Crystal.Plot2D;
+using Crystal.Plot2D.DataSources;
+using System;
+using System.Windows;
+
+namespace S001LineGraph
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      const int N = 100;
+      double[] xA = new double[N];
+      double[] yA = new double[N];
+
+      for (int i = 0; i < N; i++)
+      {
+        xA[i] = (double)i;
+        yA[i] = (double).1* Math.Sin(xA[i]);
+      }
+
+      var xDataSource = new EnumerableDataSource<double>(xA);
+      xDataSource.XMapping = x => x;
+
+      var yDataSource = new EnumerableDataSource<double>(yA);
+      yDataSource.YMapping = y => 2*y;
+
+      CompositeDataSource compositeDataSource = new CompositeDataSource(xDataSource, yDataSource);
+
+      plotter.AddLineGraph(compositeDataSource);
+
+    }
+  }
+}
